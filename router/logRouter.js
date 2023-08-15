3efc63cd8fc45413caae799b22fec0512045a336
Özgür Eskiköy <91085360+ozgureskikoy@users.noreply.github.com>
2023-08-15 15:35:15 +0300

    .

--- router/logRouter.js
+++ router/logRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 logRouter = express.Router();
-const logControls = require('../controllers/logConstrols')
+const logControls = require('../controllers/logConstroller')
 
 logRouter.get('/find', [], logControls.getLogs)
 
